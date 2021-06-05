09c326916a26937550cecf56c631cd8d6394868c
bearhug15 <iartem2000@gmail.com>
2021-06-05 11:44:57 +0700

    Delete CancelledConnectionCreation.java

--- BDFront/src/CancelledConnectionCreation.java
+++
@@ -1,9 +0,0 @@
-public class CancelledConnectionCreation extends Error {
-    String text;
-    CancelledConnectionCreation(String text){
-        this.text = text;
-    }
-    public String getText(){
-        return text;
-    }
-}
